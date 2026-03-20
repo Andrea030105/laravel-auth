f8783bb29584692ca557e8015d29b0169bdf2c9c
Andrea030105 <tummoloandrea05@gmail.com>
2026-03-20 16:05:41 +0100

    admin.index-done

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,6 +2,15 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
+        <div class="col my-3">
+            <a href="{{route('admin.projects.create')}}">
+                <button class="btn btn-square btn-primary">
+                    Add Project
+                </button>
+            </a>
+        </div>
+    </div>
+    <div class="row">
         <div class="col">
             <table class="table table-striped my-3">
                 <thead>
@@ -15,6 +24,19 @@
                     <tr>
                         <td>{{$project->title}}</td>
                         <td>{{$project->description}}</td>
+                        <td>
+                            <div class="d-flex align-items-center justify-content-center">
+                                <button class="ms-2 btn btn-square btn-primary">
+                                    <i class="fa-solid fa-eye"></i>
+                                </button>
+                                <button class="ms-2 btn btn-square btn-warning">
+                                    <i class="fa-solid fa-pencil"></i>
+                                </button>
+                                <button class="ms-2 btn btn-square btn-danger">
+                                    <i class="fa-solid fa-trash"></i>
+                                </button>
+                            </div>
+                        </td>
                     </tr>
                     @endforeach
                 </tbody>
